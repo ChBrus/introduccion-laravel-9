2d82ae2cd28a7716f3c6e1e901b4a3ecf5ae7c48
ChBrus <cbruno.carrillo@gmail.com>
2025-04-05 00:28:09 -0600

    1. Manejo de solicitudes HTTP en PHP

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Conceptos
+ * * Route::get() Consultar
+ * * Route::post() Guardar
+ * * Route::put() Actualizar
+ * * Route::delete() Eliminar
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
 });
+
+Route::get('blog', function () {
+    return 'Listado de publiocaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // Consulta a Base de Datos
+
+    return 'Publicación: ' . $slug;
+})->where('slug', '[A-Za-z0-9\-]+');
+
+Route::get('buscar', function (Request $request) {
+    // dd($request->query('q'));
+    // return 'Buscando: ' . $request->query('q');
+    return $request->all();
+})->name('buscar');
