7b2e71e04d1c192a4bedbd60a15886cc36364982
ChBrus <cbruno.carrillo@gmail.com>
2025-04-06 05:55:02 -0600

    Adding my personal icon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Web Laravel</title>
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/png">
 </head>
 <body>
     <h1>Proyecto Web Laravel</h1>
