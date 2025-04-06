e873cd7736c52aacb7b8149b5ccac93e0fd4ad7a
ChBrus <cbruno.carrillo@gmail.com>
2025-04-06 14:13:39 -0600

    6. Cómo Crear Modelos en Laravel

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $title = $this->faker->sentence(),
+            'slug' => Str::slug($title),
+            'body' => $this->faker->paragraphs(3, true)
         ];
     }
 }
